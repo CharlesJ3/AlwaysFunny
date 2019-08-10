77f4f4cbf414906c2d30f61ff0acf5295dc43f99
Charles Jones <c.jones.3.zero@gmail.com>
2019-08-10 17:30:20 -0600

    Final robofriends tutorial app push, fun times!

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
-import { robots } from './robot';
+// import { robots } from './robot';
 
 export default class App extends React.Component {  
   constructor() {
     super();
 
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
   }
 
-  onSearchChange(event){
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  }
 
-  const filteredRobots = this.state.robots.filter( robots => {
-    
-  });
-}
+  componentDidMount = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
 
   render(){
+
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    });
+
     return (
       <div className="tc">
-        <h1>Robofriends</h1>
+        <h1 className="f1 title">Robofriends</h1>
         <Searchbox searchChange={this.onSearchChange}/>
-        <CardList robots={this.state.robots} />
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
